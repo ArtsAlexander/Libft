6acd5a30b08364417a7efb488fff5148f4a46790
aarts <aarts@student.s19.be>
2021-04-27 15:38:01 +0200

    heck yeah

--- ft_substr.c
+++ ft_substr.c
@@ -1,28 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aarts <aarts@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/08 12:53:49 by aarts             #+#    #+#             */
-/*   Updated: 2021/04/27 14:25:08 by aarts            ###   ########.fr       */
+/*   Created: 2021/04/27 14:24:15 by aarts             #+#    #+#             */
+/*   Updated: 2021/04/27 15:33:30 by aarts            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t	i;
-
-	i = 0;
-	if (!src && !dst)
+	unsigned int	i;
+	unsigned int	s_len;
+	char			*str;
+	
+	str = malloc(sizeof(char) * len + 1);
+	if (!str)
 		return (0);
-	while (i < n)
+	if (!s)
+		return (0);
+	s_len = ft_strlen(s);
+	if (s_len < start)
 	{
-		*(char *)(dst + i) = *(char *)(src + i);
+		if (!(str = malloc(sizeof(char) * 1)))
+			return (NULL);
+		str[0] = '\0';
+		return (str);
+	}
+	i = 0;
+	while (i < len)
+	{
+		str[i] = s[start + i];
 		i++;
 	}
-	return (dst);
+	str[i] = 0;
+	return (str);
 }
