06b8f96edaaf9cc823f90fc4da6e4a6d78317feb
aarts <aarts@student.s19.be>
2021-04-28 17:20:59 +0200

    idk what i did

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: aarts <aarts@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/27 14:24:15 by aarts             #+#    #+#             */
-/*   Updated: 2021/04/27 17:18:18 by aarts            ###   ########.fr       */
+/*   Updated: 2021/04/28 16:30:27 by aarts            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	unsigned int	s_len;
 	char			*str;
 
-	str = malloc(sizeof(char) * len + 1);
-	if (!str || !s)
+	if (!s)
 		return (NULL);
 	s_len = ft_strlen(s);
 	if (s_len < start)
-	{
-		str = malloc(sizeof(char) * 1);
-		if (!str)
-			return (NULL);
-		str[0] = '\0';
-		return (str);
-	}
+		return (NULL);
+	str = malloc(sizeof(char) * len + 1);
+	if (!str)
+		return (NULL);
 	i = 0;
 	while (i < len)
 	{
