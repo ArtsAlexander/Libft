0308e8e24e03d45ad290f9640a22d3370bede747
Alexander Arts <aarts@student.s19.student.s>
2021-05-07 16:03:18 +0200

    split

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,34 @@
 /*   By: aarts <aarts@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/05 19:09:18 by aarts             #+#    #+#             */
-/*   Updated: 2021/05/05 19:09:31 by aarts            ###   ########.fr       */
+/*   Updated: 2021/05/07 14:58:10 by aarts            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	word_count(char const *s, char c)
+{
+	int	i;
+	int	words;
+	
+	i = 0;
+	words = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			words++;
+			while (s[i] && s[i] == c)
+				i++;
+			continue;
+		}
+		i++;
+	}
+	// if (s[i - 1] != c)
+	// 	words++;
+	return (words);
+}
 
 char	**ft_split(char const *s, char c)
 {
