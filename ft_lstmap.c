f9c5d90e285fad90a828374ec784f55970385e09
ArtsAlexander <aarts@student.s19.be>
2021-05-12 21:27:35 +0200

    e

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: aarts <aarts@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/10 18:55:22 by aarts             #+#    #+#             */
-/*   Updated: 2021/05/12 21:16:17 by aarts            ###   ########.fr       */
+/*   Updated: 2021/05/12 21:27:32 by aarts            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,8 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new_lst;
 	t_list	*new_node;
 
-	if (!f || !del)
+	if (!lst || !f)
 		return (NULL);
-	new_lst = NULL;
 	while (lst)
 	{
 		new_node = ft_lstnew(f(lst->content));
@@ -31,5 +30,6 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		ft_lstadd_back(&new_lst, new_node);
 		lst = lst->next;
 	}
+	new_lst = NULL;
 	return (new_lst);
 }
