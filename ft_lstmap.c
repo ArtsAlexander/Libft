f28218859d8fb800ad81d4d88095004e06b2ff8b
ArtsAlexander <aarts@student.s19.be>
2021-05-12 19:32:57 +0200

    ligne 23 ajouté new_node = NULL

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: aarts <aarts@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/10 18:55:22 by aarts             #+#    #+#             */
-/*   Updated: 2021/05/12 18:40:45 by aarts            ###   ########.fr       */
+/*   Updated: 2021/05/12 19:32:02 by aarts            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,21 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
-	t_list	*new_element;
+	t_list	*new_node;
 
 	if (!f || !del)
 		return (NULL);
 	new_lst = NULL;
+	new_node = NULL;
 	while (lst)
 	{
-		new_element = ft_lstnew(f(lst->content));
-		if (!new_element)
+		new_node = ft_lstnew(f(lst->content));
+		if (!new_node)
 		{
 			ft_lstclear(&new_lst, del);
 			return (NULL);
 		}
-		ft_lstadd_back(&new_lst, new_element);
+		ft_lstadd_back(&new_lst, new_node);
 		lst = lst->next;
 	}
 	return (new_lst);
