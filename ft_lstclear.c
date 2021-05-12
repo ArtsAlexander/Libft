815e830eff03d3194510e25d6dd78bc24ec2fd89
ArtsAlexander <aarts@student.s19.be>
2021-05-12 23:01:46 +0200

    a

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: aarts <aarts@student.s19.be>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/10 18:52:13 by aarts             #+#    #+#             */
-/*   Updated: 2021/05/12 22:24:20 by aarts            ###   ########.fr       */
+/*   Updated: 2021/05/12 23:01:42 by aarts            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*tmp;
 
-	if (!del)
-		return ;
 	while (*lst)
 	{
 		tmp = (*lst)->next;
